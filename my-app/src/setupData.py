de7070daecc810c26b2d55b4fef0c26d4809b158
Felix Schreyer <schreyer@fzi.de>
2022-05-13 16:04:53 +0200

    Bug Fixes surrounding traffic light application

--- my-app/src/setupData.py
+++ my-app/src/setupData.py
@@ -11,12 +11,12 @@ complete = []
 
 def get_simple_keys(datasets):
     result = []
-    for entry in datasets:
-        for key in entry.keys():
-            if type(entry[key]) != dict:
+    for dataset in datasets:
+        for key in dataset.keys():
+            if type(dataset[key]) != dict:
                 result.append(key)
             else:
-                result += get_simple_keys(entry[key])
+                result += get_simple_keys(dataset[key])
         return result
 
 
@@ -24,12 +24,14 @@ def get_simple_keys(datasets):
 with open(file_name, "r") as ds:
     data = json.load(ds)
 
-
 keys = (get_simple_keys(data))
 
-
-# get number of citations for all datasets
 for x in data:
+    '''
+    get number of citations for all datasets by accessing the semantic scholar api
+    number of citations are obtained by link of paper if it's an arxiv paper or alternatively by
+    the papers DOI
+    '''
     try:
         paper_id = x['relatedPaper']
         r = requests.get('https://api.semanticscholar.org/graph/v1/paper/URL:' + paper_id,
@@ -41,40 +43,42 @@ for x in data:
         try:
             paper_doi = x['DOI']
             r = requests.get('https://api.semanticscholar.org/graph/v1/paper/DOI:' + paper_doi,
-                                 params={'fields': 'citationCount'})
+                             params={'fields': 'citationCount'})
             # Add the number of citations to the respective dictionary
             x.update({'citationCount': r.json()['citationCount']})
         except:
             # If the request failed, citationCount is set to 0
             x.update({'citationCount': 0})
             warnings.warn(x["id"])
-
+    '''
+    check the analysis status if the dataset
+    datasets that are missing id, link to related paper and DOI if it's not an arxiv paper are considered
+    incomplete
+    '''
     for key in keys:
         # check if dataset has been provided with every property
-        if key not in x and key is not "DOI":
+        # DOI is optional and therefore excluded
+        if key not in x and key != "DOI":
             if key == "id" or key == "href":
                 incomplete.append(x['id'])
             else:
                 partiallyComplete.append(x['id'])
-                # if they aren't
         # additionally check that property hasn't been provided with an empty value either
         else:
             if key == "id" or key == "href":
                 if x[key] is None:
                     incomplete.append(x['id'])
             else:
-                if x[key] is None:
+                if key != "DOI" and x[key] is None:
                     partiallyComplete.append(x['id'])
                 else:
                     complete.append(x['id'])
-    # Check that dataset has either arxiv link or DOI
+    # Finally check whether dataset comes either with arxiv link or DOI
     try:
-        if "arxiv" not in x['href']:
-            if "DOI" not in x:
-                incomplete.append(x['id'])
+        if "arxiv" not in x['relatedPaper'] and "DOI" not in x:
+            incomplete.append(x['id'])
     except Exception as e:
         print(e)
-
 
 # remove duplicate entries from lists
 incomplete = list(dict.fromkeys(incomplete))
@@ -86,7 +90,6 @@ partiallyComplete = [i for i in partiallyComplete if i not in incomplete]
 complete = [i for i in complete if i not in incomplete]
 complete = [i for i in complete if i not in partiallyComplete]
 
-
 for entry in data:
     if entry['id'] in incomplete:
         entry.update({'completionStatus': "incomplete"})
@@ -94,7 +97,6 @@ for entry in data:
         entry.update({'completionStatus': "partially Complete"})
     elif entry['id'] in complete:
         entry.update({'completionStatus': "complete"})
-
 
 # Sort List of Dictionaries by the number of citations in descending order
 data = sorted(data, key=lambda k: k.get('citationCount'), reverse=True)

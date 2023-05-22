fc7c8ba725f8a703aad15e505efc2d784789e876
Felix Schreyer <schreyer@fzi.de>
2023-05-22 13:18:19 +0200

    adding Google Analytics

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-K1D6XN13PQ');
 
 ReactDOM.render(
   <React.StrictMode>
